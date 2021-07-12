efb2d06f6ec0210bcec7cdc9d316ebfaca3dae5f
Unknown <achrafatauil@hotmail.com>
2021-07-12 20:55:48 +0200

    add sessions & account routes for authentication

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -2,7 +2,6 @@ defmodule Dispatcher do
   use Matcher
 
   define_accept_types [
-    html: [ "text/html", "application/xhtml+html" ],
     json: [ "application/json", "application/vnd.api+json" ],
   ]
 
@@ -10,15 +9,13 @@ defmodule Dispatcher do
   @json %{ accept: %{ json: true } }
   @html %{ accept: %{ html: true } }
 
-  # In order to forward the 'themes' resource to the
-  # resource service, use the following forward rule.
-  #
-  # docker-compose stop; docker-compose rm; docker-compose up
-  # after altering this file.
-  #
-  # match "/themes/*path", @json do
-  #   Proxy.forward conn, path, "http://resource/themes/"
-  # end
+  match "/accounts/*path", %{ accept: %{ json: true } } do
+    Proxy.forward conn, path, "http://authentication/accounts/" 
+  end
+
+  match "/sessions/*path", %{ accept: %{ json: true } } do
+    Proxy.forward conn, path, "http://authentication/sessions/" 
+  end
 
   match "_", %{ last_call: true } do
     send_resp( conn, 404, "Route not found.  See config/dispatcher.ex" )
