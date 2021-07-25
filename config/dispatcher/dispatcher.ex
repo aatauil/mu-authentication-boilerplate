b8c8d88316b5afeaecbe9fab0785a5ef0b70dab0
Unknown <achrafatauil@hotmail.com>
2021-07-25 19:19:32 +0200

    Add auth , frontend & sparql routes to dispatcher

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -3,21 +3,77 @@ defmodule Dispatcher do
 
   define_accept_types [
     json: [ "application/json", "application/vnd.api+json" ],
+    html: [ "text/html", "application/xhtml+html" ],
+    sparql: [ "application/sparql-results+json" ],
+    any: [ "*/*" ]
   ]
 
-  @any %{}
-  @json %{ accept: %{ json: true } }
-  @html %{ accept: %{ html: true } }
+  define_layers [ :static, :sparql, :api_services, :frontend_fallback, :resources, :not_found ]
 
-  match "/accounts/*path", %{ accept: %{ json: true } } do
+  options "/*path", _ do
+    conn
+    |> Plug.Conn.put_resp_header( "access-control-allow-headers", "content-type,accept" )
+    |> Plug.Conn.put_resp_header( "access-control-allow-methods", "*" )
+    |> send_resp( 200, "{ \"message\": \"ok\" }" )
+  end
+
+
+  ###############
+  # STATIC
+  ###############
+  # frontend
+  match "/assets/*path", %{ layer: :static } do
+    forward conn, path, "http://frontend/assets/"
+  end
+
+  match "/index.html", %{ layer: :static } do
+    forward conn, [], "http://frontend/index.html"
+  end
+
+  match "/favicon.ico", %{ layer: :static } do
+    send_resp( conn, 404, "" )
+  end
+
+
+  ###############
+  # SPARQL
+  ###############
+  match "/sparql", %{ layer: :sparql, accept: %{ sparql: true } } do
+    forward conn, [], "http://database:8890/sparql"
+  end
+
+
+  ###############
+  # API SERVICES
+  ###############
+  post "/accounts/*path", %{ layer: :api_services, accept: %{ json: true } } do
     Proxy.forward conn, path, "http://authentication/accounts/" 
   end
 
-  match "/sessions/*path", %{ accept: %{ json: true } } do
+  delete "/accounts/current/*path", %{ layer: :api_services, accept: %{ json: true } } do
+    Proxy.forward conn, path, "http://authentication/accounts/current/" 
+  end
+
+  patch "/accounts/current/changePassword/*path", %{ layer: :api_services, accept: %{ json: true } } do
+    Proxy.forward conn, path, "http://authentication/accounts/current/changePassword/" 
+  end
+
+  match "/sessions/*path", %{ layer: :api_services, accept: %{ json: true } } do
     Proxy.forward conn, path, "http://authentication/sessions/" 
   end
 
-  match "_", %{ last_call: true } do
+
+  ###############
+  # RESOURCES
+  ###############
+  get "/accounts/*path", %{ layer: :resources, accept: %{ json: true } }  do
+    Proxy.forward conn, path, "http://resource/accounts/" 
+  end
+
+  #################
+  # NOT FOUND
+  #################
+  match "/*_path", %{ layer: :not_found } do
     send_resp( conn, 404, "Route not found.  See config/dispatcher.ex" )
   end
 
